79034a472126ca861ecb4d9b8753328ae6e5137b
Brandon271004 <utm22030600@utma.edu.mx>
2024-07-12 16:19:42 -0600

    hackaton

--- src/umbrellamx_backend/src/index.ts
+++ src/umbrellamx_backend/src/index.ts
@@ -1,10 +1,29 @@
-import { Server} from 'azle';
+import { Server } from 'azle';
 import express from 'express';
-import cors from 'cors';
 
 
 export default Server(() => {
-const app = express();
-app.use (cors());
-return app.listen()
+    const data = [
+        {
+    
+            conductividad:0
+        }
+    ]
+    const app = express();
+
+    app.post('/atrapar-datos', (req, res) => {
+        const body = req.body;
+        const new_register = {
+            conductividad:body.conductividad
+        }
+        data.push(new_register)
+
+        return res.send('ok')
+    })
+
+    app.get('/obtener-datos', (_req, res) => {
+        return res.status(200).json(data)
+    })
+    
+    return app.listen()
 })
--- src/umbrellamx_frontend/src/App.jsx
+++ src/umbrellamx_frontend/src/App.jsx
@@ -1,67 +1,40 @@
-import React, { useState, useEffect } from 'react';
+import { Client, InternetIdentity } from '@bundly/ic-core-js';
+import { IcpConnectContextProvider } from '@bundly/ic-react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import { Home } from './Home';
+import { AboutUs } from './quesomos';
 
-function App() {
-  const [ph, setPh] = useState(8);
-  const [conductividad, setConductividad] = useState({
-    valor: '',
-    salinidadPureza: '',
-    variaciones: ''
+export default function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <AboutUs />,
+    },
+    {
+      path: "/datos",
+      element: <Home />,
+    },
+  ]);
+
+  const client = Client.create({
+    restCanisters: {
+      backend: {
+        baseUrl: process.env.REACT_APP_API_REST_URL
+      }
+    },
+    providers: [
+      new InternetIdentity({
+        providerUrl: process.env.REACT_APP_INTERNET_IDENTITY_URL
+      })
+    ]
   });
 
-  useEffect(() => {
-    // Función para simular cambios en los valores cada 5 segundos
-    const interval = setInterval(() => {
-      // Simular cambios aleatorios en el pH
-      const newPh = (Math.random() * (14 - 0) + 0).toFixed(2); // Genera un pH aleatorio entre 0 y 14
-      setPh(newPh);
-
-      // Simular cambios aleatorios en la conductividad
-      const newConductividad = {
-        valor: (Math.random() * (1000 - 0) + 0).toFixed(2), // Genera un valor de conductividad aleatorio
-        salinidadPureza: Math.random() < 0.5 ? 'Agua salada' : 'Agua dulce', // Simula agua salada o dulce aleatoriamente
-        variaciones: Math.random() < 0.5 ? 'Aumentando' : 'Disminuyendo' // Simula variaciones aleatorias
-      };
-      setConductividad(newConductividad);
-    }, 5000); // Intervalo de 5000 milisegundos (5 segundos)
-
-    // Limpia el intervalo al desmontar el componente para evitar fugas de memoria
-    return () => clearInterval(interval);
-  }, []); // El array vacío asegura que useEffect se ejecute solo una vez al montar el componente
-
   return (
-    <div>
-      <header>
-        <div className='min-h-screen bg-gray-100 flex items-center justify-center'>
-          <div className='text-center rounded-xl bg-white p-8 shadow-lg max-w-3xl w-full'>
-            <h1 className='text-6xl mb-12'>Lectura de sensores</h1>
-            
-            {/* Cuadro para Sensor de pH */}
-            <div className='border-2 border-gray-300 rounded-lg p-6 mb-8'>
-              <div className='text-3xl mb-6'>
-                <p className='text-4xl mb-4'>Sensor de pH</p>
-                <div className='text-xl'>
-                  <p className='mb-2'>Valor de pH: {`${ph}`}</p>
-                  <p className='mb-2'>Tendencias y cambios: {` `}</p>
-                </div>
-              </div>
-            </div>
-            
-            {/* Cuadro para Sensor de conductividad */}
-            <div className='border-2 border-gray-300 rounded-lg p-6'>
-              <div className='text-3xl mb-6'>
-                <p className='text-4xl mb-4'>Sensor de conductividad</p>
-                <div className='text-xl'>
-                  <p className='mb-2'>Valor de conductividad: {`${conductividad.valor}`}</p>
-                  <p className='mb-2'>Salinidad y pureza: {`${conductividad.salinidadPureza}`}</p>
-                  <p className='mb-2'>Variaciones: {`${conductividad.variaciones}`}</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </header>
-    </div>
-  );
+    <IcpConnectContextProvider client={client}>
+      <RouterProvider router={router} />
+    </IcpConnectContextProvider>
+  )
 }
-
-export default App;
